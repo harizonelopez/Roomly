a31970bee1b762e536d4264cd31344cc46846215
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2025-12-25 18:07:39 +0300

    Refactor comments in the main file and the base template

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Trump&&Elon-key'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
-# Storage of active users and rooms
+# Active users & rooms storage
 active_users = {}
 chat_rooms = {'general': {'users': [], 'messages': []}}
 
